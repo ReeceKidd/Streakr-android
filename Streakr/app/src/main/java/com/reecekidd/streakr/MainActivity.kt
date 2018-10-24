595c2e795b51a2d2ff7d7a74a24b18da026074fa
Reece Kidd <reecekidd95@gmail.com>
2018-10-24 07:28:59 +0100

    chore: Made retrofit API call successfully, need to figure out how to populate list from response

--- Streakr/app/src/main/java/com/reecekidd/streakr/MainActivity.kt
+++ Streakr/app/src/main/java/com/reecekidd/streakr/MainActivity.kt
@@ -3,25 +3,36 @@ package com.reecekidd.streakr
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ListView
+import android.widget.Adapter
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import com.squareup.picasso.Picasso
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.user_item.*
+import org.w3c.dom.Text
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
-import java.text.FieldPosition
 
 class MainActivity : AppCompatActivity() {
-    lateinit var usersList: RecyclerView
-    private var BASE_URL = "10.0.2.2"
+
+    private var TAG = "TRIALCODE"
+    private var BASE_URL = "http://10.0.2.2:4040"
+    private lateinit var usersAdapter: UsersAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        usersList.adapter
+        usersAdapter = UsersAdapter()
         val retrofit: Retrofit = Retrofit.Builder()
                 .baseUrl(BASE_URL)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
 
@@ -30,30 +41,46 @@ class MainActivity : AppCompatActivity() {
                 .subscribeOn(Schedulers.io())
                 .unsubscribeOn(Schedulers.computation())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({
-                    var usersResponse = it
-                },{
-
-                })
+                .subscribe({usersAdapter.setUsers(it.data)}, {
+                    Toast.makeText(applicationContext, it.message, Toast.LENGTH_SHORT).show()
+                }).dispose()
+        Log.v(TAG, usersAPI.toString())
 
     }
 
-//    inner class UsersListAdapter : RecyclerView.Adapter<UsersListAdapter.UserViewHolder>() {
-//
-//        private val users: List<User> = mutableListOf()
-//
-//        override fun getItemCount(): Int {
-//            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-//        }
-//
-//        override fun onBindViewHolder(holder: UserViewHolder, position: Int) {}
-//        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
-//            return UserViewHolder(layoutInflater.inflate(R.layout.user_item))
-//        }
-//
-//        inner class UserViewHolder(val itemView: View): RecyclerView.ViewHolder()
-//
-//    }
+    inner class UsersAdapter : RecyclerView.Adapter<UsersAdapter.UserViewHolder>() {
+
+        private val users: MutableList<User> = mutableListOf()
+
+        override fun getItemCount(): Int {
+            return users.size
+        }
+
+        override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+            holder.bindModel(users[position])
+        }
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+            return UserViewHolder(layoutInflater.inflate(R.layout.user_item, parent, false))
+        }
+
+        fun setUsers(data: List<User>){
+            users.addAll(data)
+            notifyDataSetChanged()
+        }
+
+        inner class UserViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+
+            val userName: TextView = itemView.findViewById(R.id.userName)
+            val userStreaks: TextView = itemView.findViewById(R.id.userStreaks)
+            val userAvatar: ImageView = itemView.findViewById(R.id.userAvatar)
+            fun bindModel(user: User){
+                userName.text = "${user.firstName} ${user.lastName}"
+                userStreaks.text = user.streaks.toString()
+                Picasso.get().load(user.profilePicture).into(userAvatar)
+            }
+        }
+
+    }
 
 
 }
--- Streakr/app/src/main/java/com/reecekidd/streakr/User.kt
+++ Streakr/app/src/main/java/com/reecekidd/streakr/User.kt
@@ -1,3 +1,3 @@
 package com.reecekidd.streakr
 
-data class User(val firstName: String, val lastName: String, val userName: String, val email: String, val name: String, val streaks: Array<Streak>)
+data class User(val firstName: String, val lastName: String, val userName: String, val email: String, val name: String, val streaks: Array<Streak>, val profilePicture: String)
