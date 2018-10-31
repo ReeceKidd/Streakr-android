8ff999ccaa4992843011bf5c73c1c7c55ebab5fb
Reece Kidd <reecekidd95@gmail.com>
2018-10-31 07:24:01 +0000

    fix: Got default avatar image to display correctly

--- Streakr/app/src/main/java/com/reecekidd/streakr/UserAdapter.kt
+++ Streakr/app/src/main/java/com/reecekidd/streakr/UserAdapter.kt
@@ -4,11 +4,12 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.user_row.view.*
 
 class UserAdapter : RecyclerView.Adapter<UserViewHolder>() {
 
     override fun getItemCount(): Int {
-        return 3
+        return 4
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
@@ -18,7 +19,8 @@ class UserAdapter : RecyclerView.Adapter<UserViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
-
+        holder.itemView.userFullName.text = "123"
+        holder.itemView.userName.text = "TEST"
     }
 }
 
