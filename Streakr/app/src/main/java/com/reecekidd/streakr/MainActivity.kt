53e013d396b945a7a3ab94765828fa95eecf8e80
Reece Kidd <reecekidd95@gmail.com>
2018-11-05 07:28:49 +0000

    refactor: Removed unnecessary class

--- Streakr/app/src/main/java/com/reecekidd/streakr/MainActivity.kt
+++ Streakr/app/src/main/java/com/reecekidd/streakr/MainActivity.kt
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
+import android.widget.Toast
 import com.google.gson.GsonBuilder
 import com.reecekidd.streakr.User.UserAdapter
 import com.reecekidd.streakr.User.UserFeed
