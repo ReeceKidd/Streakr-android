815a02a44a812d410d7398a3fc216180122ddb15
Reece Kidd <reecekidd95@gmail.com>
2019-01-04 06:46:41 +0000

    feat: Linked registration API to registration activity

--- Streakr/app/src/main/java/com/reecekidd/streakr/RegisterActivity.kt
+++ Streakr/app/src/main/java/com/reecekidd/streakr/RegisterActivity.kt
@@ -4,29 +4,69 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log
+import com.google.gson.GsonBuilder
+import com.reecekidd.streakr.User.UserAdapter
+import com.reecekidd.streakr.User.UserFeed
 
 import kotlinx.android.synthetic.main.activity_register.*
+import kotlinx.android.synthetic.main.activity_user_feed.*
+import okhttp3.*
+import java.io.IOException
 
 class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
-        val tag = "Login activity"
+        val tag = "Register Activity"
 
         loginTextView.setOnClickListener {
+            Log.d(tag, "Login button tclicked")
             val intent = Intent(this.applicationContext, LoginActivity::class.java)
             this.applicationContext.startActivity(intent)
         }
 
         registerButton.setOnClickListener {
+            Log.d(tag, "Registration button clicked")
             val userNameText = userNameTextField.text.toString()
             val emailText = emailTextField.text.toString()
             val passwordText = passwordTextField.text.toString()
             Log.d(tag, "Username: $userNameText")
             Log.d(tag, "Email $emailText")
             Log.d(tag, "Password $passwordText")
+            makeRegistrationCall(userNameText, emailText, passwordText)
         }
+
+
+    }
+
+    private fun makeRegistrationCall(userNameText: String, emailText: String, passwordText: String) {
+        Log.d("Registration", "Attempting to register user")
+
+        val url = "http://10.0.2.2:4040/user/register"
+
+        val json = """ {
+ 	    "userName": "$userNameText",
+          "email": "$emailText",
+             "password": "$passwordText"
+            }
+        	""".trimIndent()
+        Log.d("Registration", "json: $json")
+        val body = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), json)
+
+        val request = Request.Builder().url(url).post(body).build()
+        val client = OkHttpClient()
+        client.newCall(request).enqueue(object : Callback {
+            override fun onResponse(call: Call, response: Response) {
+                val body = response?.body()?.string()
+                Log.d("Registration", "Body : $body")
+            }
+
+            override fun onFailure(call: Call, e: IOException) {
+                Log.d("Registration", "Failed to execute request ${e.message}")
+            }
+        })
+
 
     }
 
