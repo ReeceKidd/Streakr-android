5979ccc5ee39ea5d5bf84c2696f6a6df6f68363d
Reece Kidd <reecekidd95@gmail.com>
2019-02-02 17:09:11 +0000

    feat: Began work on create a streak screen

---
+++ Streakr/app/src/main/java/com/reecekidd/streakr/CreateStreak.kt
@@ -0,0 +1,16 @@
+package com.reecekidd.streakr
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+
+import kotlinx.android.synthetic.main.activity_create_streak.*
+
+class CreateStreak : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_streak)
+    }
+
+}
