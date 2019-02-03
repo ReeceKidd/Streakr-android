68f76fb54d5ec7307c852e896f7ebfaa84229e3e
Reece Kidd <reecekidd95@gmail.com>
2019-02-03 13:09:19 +0000

    feat: Linked home screen to createAStreak activity

--- Streakr/app/src/main/java/com/reecekidd/streakr/HomeActivity.kt
+++ Streakr/app/src/main/java/com/reecekidd/streakr/HomeActivity.kt
@@ -1,5 +1,6 @@
 package com.reecekidd.streakr
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
@@ -11,6 +12,11 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        createAStreakButton.setOnClickListener {
+            val intent = Intent(this.applicationContext, CreateStreak::class.java)
+            this.applicationContext.startActivity(intent)
+        }
     }
 
 }
