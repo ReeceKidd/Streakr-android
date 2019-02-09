9ce8a85e01729f74567214ee10baeebc7d71d6dc
Reece Kidd <reecekidd95@gmail.com>
2019-02-09 16:26:54 +0000

    feat: Created StreakCreated activity

--- Streakr/app/src/main/java/com/reecekidd/streakr/SoloForfeitActivity.kt
+++ Streakr/app/src/main/java/com/reecekidd/streakr/SoloForfeitActivity.kt
@@ -1,9 +1,12 @@
 package com.reecekidd.streakr
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log
 import com.reecekidd.streakr.R
+import kotlinx.android.synthetic.main.activity_register.*
 
 import kotlinx.android.synthetic.main.activity_solo_forfeit.*
 
@@ -12,6 +15,16 @@ class SoloForfeitActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_solo_forfeit)
+
+        soloForfeitSkipButton.setOnClickListener {
+            val soloRewardActivityIntent = Intent(this.applicationContext, SoloRewardActivity::class.java)
+            this.applicationContext.startActivity(soloRewardActivityIntent)
+        }
+
+        soloForfeitSubmitButton.setOnClickListener {
+            val soloRewardActivityIntent = Intent(this.applicationContext, SoloRewardActivity::class.java)
+            this.applicationContext.startActivity(soloRewardActivityIntent)
+        }
     }
 
 }
--- Streakr/app/src/main/java/com/reecekidd/streakr/SoloRewardActivity.kt
+++ Streakr/app/src/main/java/com/reecekidd/streakr/SoloRewardActivity.kt
@@ -1,8 +1,10 @@
 package com.reecekidd.streakr
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
+import kotlinx.android.synthetic.main.activity_solo_forfeit.*
 
 import kotlinx.android.synthetic.main.activity_solo_reward.*
 
@@ -11,6 +13,15 @@ class SoloRewardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_solo_reward)
+        soloRewardSkipButton.setOnClickListener {
+            val streakCreatedIntent = Intent(this.applicationContext, StreakCreatedActivity::class.java)
+            this.applicationContext.startActivity(streakCreatedIntent)
+        }
+
+        soloRewardSubmitButton.setOnClickListener {
+            val streakCreatedIntent = Intent(this.applicationContext, StreakCreatedActivity::class.java)
+            this.applicationContext.startActivity(streakCreatedIntent)
+        }
     }
 
 }
