ce71647bae8c6d206e4aacdb233a1ee686dd6b6d
Reece Kidd <reecekidd95@gmail.com>
2019-03-19 07:24:48 +0000

    feat: User can create solo streak and return to the home page

--- Streakr/app/src/main/java/com/reecekidd/streakr/SoloStreakCreatedActivity.kt
+++ Streakr/app/src/main/java/com/reecekidd/streakr/SoloStreakCreatedActivity.kt
@@ -1,8 +1,10 @@
 package com.reecekidd.streakr
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
+import kotlinx.android.synthetic.main.activity_create_solo_streak.*
 
 import kotlinx.android.synthetic.main.activity_solo_streak_created.*
 
@@ -15,6 +17,11 @@ class SoloStreakCreatedActivity : AppCompatActivity() {
         val streakDescription: String = intent.getStringExtra(CreateSoloStreak.SoloStreak.STREAK_DESCRIPTION)
         createdSoloStreakName.setText(streakName)
         createdSoloStreakDescription.setText(streakDescription)
+
+        soloStreakCreatedHomeButton.setOnClickListener {
+            val intent = Intent(this.applicationContext, HomeActivity::class.java)
+            this.applicationContext.startActivity(intent)
+        }
     }
 
 }
