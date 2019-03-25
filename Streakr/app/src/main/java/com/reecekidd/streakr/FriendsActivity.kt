98db0500a347c6a1e12946a3d89349a39e37a586
Reece Kidd <reecekidd95@gmail.com>
2019-03-25 07:23:20 +0000

    feat: Added friendToolbar and linked addFriend menu item to addFriend menu activity

--- Streakr/app/src/main/java/com/reecekidd/streakr/FriendsActivity.kt
+++ Streakr/app/src/main/java/com/reecekidd/streakr/FriendsActivity.kt
@@ -1,8 +1,13 @@
 package com.reecekidd.streakr
 
+import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+
 
 import kotlinx.android.synthetic.main.activity_friends.*
 
@@ -11,6 +16,24 @@ class FriendsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_friends)
+        setSupportActionBar(findViewById(R.id.friendsToolbar))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.friends, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?) = when(item?.itemId){
+        R.id.addFriend -> {
+            val intent = Intent(this.applicationContext, AddFriendActivity::class.java)
+            this.applicationContext.startActivity(intent)
+            true
+        }
+
+        else -> {
+            super.onOptionsItemSelected(item)
+        }
     }
 
 }
