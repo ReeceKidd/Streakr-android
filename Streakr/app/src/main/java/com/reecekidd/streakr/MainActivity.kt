a33214323cb6fbdb4a40c6eea33d64d82a839c3c
Reece Kidd <reecekidd95@gmail.com>
2019-04-06 17:30:19 +0100

    refactor: Updated MainActivity to be completed inside of an AsyncTask

--- Streakr/app/src/main/java/com/reecekidd/streakr/MainActivity.kt
+++ Streakr/app/src/main/java/com/reecekidd/streakr/MainActivity.kt
@@ -2,6 +2,7 @@ package com.reecekidd.streakr
 
 import android.content.Context
 import android.content.Intent
+import android.os.AsyncTask
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
@@ -15,32 +16,33 @@ class MainActivity : AppCompatActivity() {
 
     val tag = "MainActivity"
 
+    lateinit var context: Context
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val sharedPreferences = getSharedPreferences(
-                getString(R.string.shared_preferences_api_key), Context.MODE_PRIVATE)
-        val jsonWebToken = sharedPreferences.getString(getString(R.string.json_web_token), null)
-        if(jsonWebToken != null && jsonWebToken.isNotEmpty()){
-            Log.d(tag, "jsonWebToken is not empty")
-            val homeAcvitityIntent = Intent(this.applicationContext, HomeActivity::class.java)
-            this.applicationContext.startActivity(homeAcvitityIntent)
-            return
+        context = this.applicationContext
+        jsonWebTokenManagerTask().execute()
+    }
+
+    internal inner class jsonWebTokenManagerTask: AsyncTask<Void, Void, String>() {
+        override fun doInBackground(vararg params: Void?): String? {
+            val sharedPreferences = getSharedPreferences(
+                    getString(R.string.shared_preferences_api_key), Context.MODE_PRIVATE)
+            val jsonWebToken = sharedPreferences.getString(getString(R.string.json_web_token), null)
+            if(jsonWebToken != null && jsonWebToken.isNotEmpty()){
+                Log.d(tag, "jsonWebToken is not empty")
+                // THIS IS WHERE I NEED TO CALL AND VERIFY IF JSON WEB TOKEN IS CORRECT
+                val homeActivityIntent = Intent(context, HomeActivity::class.java)
+                context.startActivity(homeActivityIntent)
+                return null
+            }
+
+            Log.d(tag, "startingRegisterActivity")
+            val registerActivityIntent = Intent(context, RegisterActivity::class.java)
+            context.startActivity(registerActivityIntent)
+            return "Success"
         }
-
-        Log.d(tag, "startingRegisterActivity")
-        val registerAcvitityIntent = Intent(this.applicationContext, RegisterActivity::class.java)
-        this.applicationContext.startActivity(registerAcvitityIntent)
-    }
-
-    override fun onStart() {
-        super.onStart()
-        Log.d("MainActivity", "onstart")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        Log.d("MainActivity", "onRestart")
     }
 
 }
